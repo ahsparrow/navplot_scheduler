eaaf9c5e6118ce9a4efd866ea4064fcf828d325d
Alan Sparrow <git@freeflight.org.uk>
2022-09-03 11:02:47 +0100

    Add github action

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 /**
- * Welcome to Cloudflare Workers! This is your first scheduled worker.
- *
  * - Run `wrangler dev --local` in your terminal to start a development server
  * - Run `curl "http://localhost:8787/cdn-cgi/mf/scheduled"` to trigger the scheduled event
  * - Go back to the console to see what your worker has logged
  * - Update the Cron trigger in wrangler.toml (see https://developers.cloudflare.com/workers/wrangler/configuration/#triggers)
  * - Run `wrangler publish --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
  */
 
 export default {
-	async scheduled(controller, env, ctx) {
-		console.log(`Hello World!`);
-	},
+  async scheduled(controller, env, ctx) {
+    console.log('Scheduled request');
+
+    const init = {
+      method: 'POST',
+      headers: {
+        'User-Agent': 'ahsparrow',
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': env.GITHUB_TOKEN
+      },
+      body: JSON.stringify({"ref": "refs/heads/master"})
+    };
+
+    const response = await fetch("https://api.github.com/repos/ahsparrow/navplot2/actions/workflows/navplot.yaml/dispatches", init)
+     .then((response) => response.text())
+     .then((txt) => console.log(txt));
+  },
 };
